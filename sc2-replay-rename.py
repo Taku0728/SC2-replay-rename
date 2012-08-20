c1b52e50b0f3adc65721249e8b402681dd7a9cef
Patrick Donnelly <batrick@batbytes.com>
2012-08-20 16:22:54 -0400

    more work on this

--- sc2-replay-rename.py
+++ sc2-replay-rename.py
@@ -2,31 +2,39 @@
 
 import argparse
 import os
+import re
 import sc2reader
 import sys
 
-DEFAULT_FORMAT = ""
+# 2012-03-29 23-07-34 PvT batrick (P) vs Fuzzzy (T) Ohana LE.SC2Replay
+DEFAULT_FORMAT = "/date/ /time/ /matchup/ /ateam/ vs /bteam/ /map/.SC2Replay"
 
 parser = argparse.ArgumentParser(description = "Format Starcraft II Replay File Name.")
 parser.add_argument("-f", "--format", nargs = 1, default = DEFAULT_FORMAT, help = "format for the filename", dest = "format")
+#parser.add_argument("-l", "--ladder", default = False, help = "rename ladder replays", dest = "ladder")
+#parser.add_argument("-m", "--melee", default = False, help = "rename melee custom replays", dest = "melee")
 parser.add_argument("files", nargs = "+", help = "files to rename", metavar = "file")
 args = parser.parse_args()
 
-
 options = {
-  "/ateam/": 1,
-  "/bteam/": 1,
+  "/ateam/": lambda (replay): "ateam",
+  "/bteam/": lambda (replay): "bteam",
   "/category/": lambda (replay): replay.category,
-  "/date/": lambda (replay): replay.date,
-  "/length/": lambda (replay): replay.length,
+  "/date/": lambda (replay): "date",
+#"/date/": lambda (replay): replay.date.strftime("%Y-%m-%d"),
+  "/length/": lambda (replay): "0", #replay.length,
   "/map/": lambda (replay): replay.map,
   "/matchup/": lambda (replay): replay.type,
+  "/time/": lambda (replay): "0", #replay.length,
   "/type/": lambda (replay): replay.type,
   "/version/": lambda (replay): replay.version,
 }
 
+pattern = re.compile("/[^/]*/")
 def replay_filename (replay):
-    return "bar" # + .SC2Replay
+    def replace (option):
+        return options[option.group(0)](replay)
+    return pattern.sub(replace, args.format)
 
 for f in args.files:
     replay = sc2reader.load_replay(f)
@@ -34,4 +42,5 @@ for f in args.files:
     if os.access(nf, os.F_OK):
         print("cannot rename replay `%s' to `%s': destination exists" % (f, nf))
         sys.exit(1);
-    os.rename(f, replay_filename(replay))
+    sys.stderr.write("`%s' -> `%s'\n" % (f, nf));
+    # os.rename(f, replay_filename(replay))
