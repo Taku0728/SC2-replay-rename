0dbefb86cd5479944da6672cdb96e0c2bf831d78
Patrick Donnelly <batrick@batbytes.com>
2012-08-21 23:52:46 -0400

    dates/times

--- sc2-replay-rename.py
+++ sc2-replay-rename.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python2
 
 import argparse
+import datetime
 import os
 import re
 import sc2reader
 import sys
+import time
 
 # 2012-03-29 23-07-34 PvT batrick (P) vs Fuzzzy (T) Ohana LE.SC2Replay
 DEFAULT_FORMAT = "/date/ /time/ /matchup/ /ateam/ vs /bteam/ /map/.SC2Replay"
 
 parser = argparse.ArgumentParser(description = "Format Starcraft II Replay File Name.")
 parser.add_argument("-f", "--format", nargs = 1, default = DEFAULT_FORMAT, help = "format for the filename", dest = "format")
-#parser.add_argument("-l", "--ladder", default = False, help = "rename ladder replays", dest = "ladder")
-#parser.add_argument("-m", "--melee", default = False, help = "rename melee custom replays", dest = "melee")
+#parser.add_argument("-l", "--ladder", action = "store_true", default = False, help = "rename ladder replays", dest = "ladder")
+#parser.add_argument("-m", "--melee", action = "store_true", default = False, help = "rename melee custom replays", dest = "melee")
 parser.add_argument("files", nargs = "+", help = "files to rename", metavar = "file")
 args = parser.parse_args()
 
+# TODO adjust date to account for DST
 options = {
   "/ateam/": lambda (replay): "ateam",
   "/bteam/": lambda (replay): "bteam",
   "/category/": lambda (replay): replay.category,
-  "/date/": lambda (replay): "date",
-#"/date/": lambda (replay): replay.date.strftime("%Y-%m-%d"),
+  "/date/": lambda (replay): datetime.datetime.fromtimestamp(float(replay.date.strftime("%s"))).strftime("%Y-%m-%d"),
   "/length/": lambda (replay): "0", #replay.length,
-  "/map/": lambda (replay): replay.map,
+  "/map/": lambda (replay): replay.map_name,
   "/matchup/": lambda (replay): replay.type,
-  "/time/": lambda (replay): "0", #replay.length,
+  "/time/": lambda (replay): datetime.datetime.fromtimestamp(float(replay.date.strftime("%s"))).strftime("%H-%M-%S"),
   "/type/": lambda (replay): replay.type,
+  "/utcdate/": lambda (replay): replay.date.strftime("%Y-%m-%d"),
+  "/utctime/": lambda (replay): replay.date.strftime("%H-%M-%S"),
   "/version/": lambda (replay): replay.version,
 }
 
